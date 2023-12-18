384be21f38c998b94575a907a65e4401e024bf47
ToCaveman <peterapasta2@gmail.com>
2023-12-18 19:36:43 +0200

    tagad pateisam atbild nevis tikai @mention

--- src/events/messageCreate/replyToUser.js
+++ src/events/messageCreate/replyToUser.js
@@ -3,12 +3,14 @@
 //1039086985907806220 - svaistamize
 //897740828389302303 - rudis
 //297710605522173954 - markus
+//1185565334049128509 - testa id
 
 const { Client, Message } = require("discord.js");
 const targetClientIDs = [
 	"1039086985907806220",
 	"897740828389302303",
 	"297710605522173954",
+	"1185565334049128509",
 ];
 var replies = [
 	"klusu objekt",
@@ -29,6 +31,6 @@ var replies = [
 module.exports = async (client, message) => {
 	if (targetClientIDs.includes(message.author.id)) {
 		var randReply = replies[(Math.random() * replies.length) | 0];
-		message.channel.send(`${message.member} ${randReply}`);
+		message.reply(`${message.member} ${randReply}`);
 	}
 };
